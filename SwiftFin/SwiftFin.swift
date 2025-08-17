2ee12aeecc1541758eb73095e68f4572c83b21e0
Effy <fatimam2000@gmail.com>
2025-08-17 16:26:37 -0400

    Initial Commit

---
+++ SwiftFin/SwiftFin.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftFin.swift
+//  SwiftFin
+//
+//  Created by Effy on 2025-08-17.
+//
+
+import Foundation
+
---
+++ SwiftFinTests/SwiftFinTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFinTests.swift
+//  SwiftFinTests
+//
+//  Created by Effy on 2025-08-17.
+//
+
+import Testing
+@testable import SwiftFin
+
+struct SwiftFinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
